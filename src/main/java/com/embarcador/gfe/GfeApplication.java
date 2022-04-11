ee462bed79c6a35078433309207ae9f8498d5f4e
Jefferson Hita <jefflaiho@hotmail.com>
2022-04-11 15:24:37 -0300

    Projeto Criado

---
+++ src/main/java/com/embarcador/gfe/GfeApplication.java
@@ -0,0 +1,13 @@
+package com.embarcador.gfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/embarcador/gfe/GfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.embarcador.gfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
